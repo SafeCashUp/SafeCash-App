0a87e958b7be68bdc1e2febe9c43a90339a5eae0
vitudominguess <vitorpessoadomingues@hotmail.com>
2021-12-03 23:53:42 +0000

    Change style drawer

--- SafeCash/src/components/RotasDrawer.js
+++ SafeCash/src/components/RotasDrawer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Text } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 import Inicial from '../views/Inicial/Inicial';
 import Sobre from '../views/Sobre/Sobre'
 
@@ -7,14 +9,25 @@ const Drawer = createDrawerNavigator();
 
 const RotasDrawer = () => {
     return (
-        <Drawer.Navigator initialRouteName="InicialIn">
+        <Drawer.Navigator
+          initialRouteName="InicialIn"
+          screenOptions={{
+            drawerStyle: {
+              backgroundColor: 'gainsboro',
+              paddingVertical: 20
+            },
+            drawerActiveBackgroundColor: 'silver'
+          }}
+          >
           <Drawer.Screen
               name="InicialIn"
               component={Inicial}
               options={
                 { headerStyle: { backgroundColor: '#151515'},
                   headerTintColor: 'gainsboro',
-                  title: 'Home'
+                  title: 'Home',
+                  drawerLabel: (({focused}) => <Text style={{color: focused ? '#FF781F' : '#151515' }}>Home</Text>),
+                  drawerIcon: (({focused}) => <AntDesign color={focused ? '#FF781F' : '#151515'} size={20} name="home"/>)
                 }
               }
           />
@@ -23,7 +36,9 @@ const RotasDrawer = () => {
               component={Sobre}
               options={
                 { headerStyle: { backgroundColor: '#151515'},
-                  headerTintColor: 'gainsboro'
+                  headerTintColor: 'gainsboro',
+                  drawerLabel: (({focused}) => <Text style={{color: focused ? '#FF781F' : '#151515' }}>Sobre</Text>),
+                  drawerIcon: (({focused}) => <AntDesign color={focused ? '#FF781F' : '#151515'} size={20} name="exclamationcircleo"/>)
                 }
               }
           />
